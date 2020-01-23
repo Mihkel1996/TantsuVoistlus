dc9666b3b69a617137c1402395c6fc1d9be8ebdd
Opilane TTHK <opilane@tthk.ee>
2020-01-23 13:45:08 +0200

    Add project files.

---
+++ TantsuVoistlus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TantsuVoistlus.Startup))]
+namespace TantsuVoistlus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
